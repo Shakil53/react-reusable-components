e2b78c0b924f80c4e39451344f474663124a6acf
Shakil53 <shakilahmed.dcc@gmail.com>
2024-02-24 00:06:43 +0600

    framer motion stagger children

--- src/components/FramerMotion/FramerMotion.tsx
+++ src/components/FramerMotion/FramerMotion.tsx
@@ -1,15 +1,43 @@
 import {motion} from 'framer-motion'
 
 const FramerMotion = () => {
-    const initial = { rotate: 0 };
-    const animate = { rotate: 360 };
+  
+
+    const parent = {
+        hidden : { opacity: 0, scale: 0.9 },
+        visible : { opacity: 1, scale: 1 },
+    }
+
+    const child = {
+        hidden : { opacity: 0, scale: 0.1 },
+        visible : { opacity: 1, scale: 1 },
+    }
+
     return (
         <div>
             <motion.div
-                initial={initial}
-                animate={animate}
-                transition={{type: "spring", duration: 2}}
-                className='size-64 bg-indigo-500 rounded-lg mt-11 flex justify-center items-center'></motion.div>
+                variants={parent}
+                initial= "hidden"
+                animate= "visible"
+                transition={{
+                    ease: 'easeInOut', duration: 1.5, delayChildren:0.5,
+                staggerChildren: 0.5}}
+                className='size-64 flex flex-wrap gap-5 p-3 bg-indigo-500 rounded-lg justify-center items-center mt-11'>
+                
+                <motion.div className='size-20 bg-cyan-500 rounded-lg'
+                    variants={child}
+                   
+                ></motion.div>
+                <motion.div className='size-20 bg-cyan-500 rounded-lg'
+                variants={child}
+               ></motion.div>
+                <motion.div className='size-20 bg-cyan-500 rounded-lg'
+                variants={child}
+                ></motion.div>
+                <motion.div className='size-20 bg-cyan-500 rounded-lg'
+                variants={child}
+                ></motion.div>
+                </motion.div>
         </div>
     );
 };
