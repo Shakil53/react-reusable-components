bfefa98003a11f316b128288dce38e9df6f57215
Shakil53 <shakilahmed.dcc@gmail.com>
2024-03-04 10:44:51 +0600

    useMotionValue with transform scale, rotate, opacity

--- src/components/FramerMotionHooks/UseMotionValue.tsx
+++ src/components/FramerMotionHooks/UseMotionValue.tsx
@@ -1,13 +1,17 @@
 import { motion, useMotionValue, useTransform } from 'framer-motion';
 const UseMotionValue = () => {
     const x = useMotionValue(0);
-    const opacity = useTransform(x, [0, 100],[1, 0])
+    // const opacity = useTransform(x, [0, 500], [1, 0])
+    const scale = useTransform(x, [-200, 200], [0.5, 1.5]);
+    const rotate = useTransform(x, [-200, 200], [0, 180]);
     return (
         <div className='border border-red-500 size-[600px] flex flex-col justify-center'>
             <motion.div
                 className='size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center'
-                style={{ x, opacity }}
+                style={{ x, scale, rotate }}
                 drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragSnapToOrigin
             >
                 
             </motion.div>
